68203f2aae164bda36f01512cb3cd88a13864d1b
Bruno Vecchi <vecchi.b@gmail.com>
2009-05-19 23:21:15 -0300

    de-declared the rest of them

--- lib/Proteolysis/DB.pm
+++ lib/Proteolysis/DB.pm
@@ -1,17 +1,20 @@
-use MooseX::Declare;
+package Proteolysis::DB;
+use Moose;
+extends 'KiokuX::Model';
+use KiokuDB::TypeMap;
+use KiokuDB::TypeMap::Entry::Naive;
+use namespace::clean -except => 'meta';
 
-class Proteolysis::DB extends KiokuX::Model {
-    use KiokuDB::TypeMap;
-    use KiokuDB::TypeMap::Entry::Naive;
+has '+extra_args' => (
+    default => sub { {
+        create  => 1,
+        typemap => KiokuDB::TypeMap->new(
+            isa_entries => {
+                'Bio::Root::Root' => KiokuDB::TypeMap::Entry::Naive->new,
+            }
+        ),
+    } }
+);
 
-    has '+extra_args' => (
-        default => sub { {
-            create  => 1,
-            typemap => KiokuDB::TypeMap->new(
-                isa_entries => {
-                    'Bio::Root::Root' => KiokuDB::TypeMap::Entry::Naive->new,
-                }
-            ),
-        } }
-    );
-}
+__PACKAGE__->meta->make_immutable;
+1;
