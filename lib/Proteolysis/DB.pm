2d62675e28e59f73a648cd42211bb73d7ead8822
Bruno Vecchi <vecchi.b@gmail.com>
2009-06-16 18:58:16 -0300

    add indexing code

--- lib/Proteolysis/DB.pm
+++ lib/Proteolysis/DB.pm
@@ -38,6 +38,54 @@ has '+typemap' => (
     }
 );
 
+sub BUILDARGS {
+    my ($class, %args) = @_;
+
+    my $columns = [
+    # specify extra columns for the 'entries' table
+        protease => {
+            data_type   => 'varchar',
+            is_nullable => 1,
+            extract => sub {
+                my $obj = shift;
+                if (ref $obj eq 'Bio::Protease') {
+                    return $obj->name;
+                }
+            },
+        },
+
+        detail_level => {
+            data_type   => 'varchar',
+            is_nullable => 1,
+        },
+
+        dh => {
+            data_type   => 'real',
+            is_nullable => 1,
+        },
+
+        mean_inverse_ace => {
+            data_type   => 'real',
+            is_nullable => 1,
+        },
+
+        mean_length => {
+            data_type   => 'real',
+            is_nullable => 1,
+        },
+
+        substrate_count => {
+            data_type   => 'integer',
+            is_nullable => 1,
+        }
+
+    ];
+
+    $args{extra_args}->{columns} = $columns;
+
+    return $class->SUPER::BUILDARGS(%args);
+}
+
 has '+extra_args' => (
     default => sub { {
         create  => 1,
